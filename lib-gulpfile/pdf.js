3f680e8cec726235c3e5e0c1f7a9de2605f4c015
Kevin Brightwell <kevin.brightwell2@gmail.com>
2017-01-12 08:07:26 -0500

    Fix issue with PDF tasks

--- lib-gulpfile/pdf.js
+++ lib-gulpfile/pdf.js
@@ -2,7 +2,10 @@
 
 const path = require('path');
 const config = require('./config');
-const util = require('./util');
+const util = require('../lib/util');
+
+const gulp = require('gulp');
+const $ = require('gulp-load-plugins')();
 
 const _ = require('lodash');
 const gutil = require('gulp-util');
@@ -32,63 +35,57 @@ function getRangeArg(variable) {
   }
 }
 
-const TASK_NAME = 'pdfs';
-module.exports = (gulp, $) => {
+let options = _.cloneDeep(config.decktape);
 
-  let options = _.cloneDeep(config.decktape);
+if (gutil.env['remote']) {
+  options.hrefBase = gutil.env['remote'];
+}
 
-  if (gutil.env['remote']) {
-    options.hrefBase = gutil.env['remote'];
-  }
+const exclude = getRangeArg('x');
+const lectures = _.filter(getRangeArg('r'), v => (_.indexOf(exclude, v) == -1))
+  .map(l => (config.sys.src.lectures(`**/${padLeft(l, 2, '0')}-**`)));
 
-  const exclude = getRangeArg('x');
-  const lectures = _.filter(getRangeArg('r'), v => (_.indexOf(exclude, v) == -1))
-    .map(l => (config.sys.src.lectures(`**/${padLeft(l, 2, '0')}-**`)));
+const pugs = (lectures.length > 0 ? lectures.map(p => (p + '.pug')) : [config.sys.src.lectures('**/*.pug')]);
 
-  const pugs = (lectures.length > 0 ? lectures.map(p => (p + '.pug')) : [config.sys.src.lectures('**/*.pug')]);
+function savePDFs(cb) {
 
-  function savePDFs(cb) {
+  const TEMPLATE = _.template('<%= decktape.phantomjs %> <%= decktape.js %> ' +
+    'reveal <%= decktape.hrefBase %>');
 
-    const TEMPLATE = _.template('<%= decktape.phantomjs %> <%= decktape.js %> ' +
-      'reveal <%= decktape.hrefBase %>');
+  pump([
+    gulp.src(pugs),
+    $.filter(['**', `!**/*.@(layout|component).pug`]),
+    $.debug({ title: 'Saving Lecture:' }),
+    $.rename({ extname: '' }),
+    $.exec(TEMPLATE({ decktape: options }) + '<%= options.htmlPath(file.path) %>?printing <%=options.pdfPath(file.path) %>',
+      {
+        cwd: options.srcDir,
+        htmlPath: input => (util.htmlPathJoin(path.relative(config.sys.src(), input))),
+        pdfPath: function (input) {
+          const split = input.split(path.sep);
+          split.splice(split.length - 1, 0, 'pdf');
+          const out = path.relative('.' + path.sep + options.srcDir, split.join(path.sep) + '.pdf');
+          return out;
+        }
+      }),
+    $.exec.reporter({ stdout: true }),
+    $.debug({ title: 'Finished Lecture:' }),
+  ], cb);
+}
 
-    pump([
-      gulp.src(pugs),
-      $.filter(['**', `!**/*.@(layout|component).pug`]),
-      $.debug({ title: 'Saving Lecture:' }),
-      $.rename({ extname: '' }),
-      $.exec(TEMPLATE({ decktape: options }) + '<%= options.htmlPath(file.path) %>?printing <%=options.pdfPath(file.path) %>',
-        {
-          cwd: options.srcDir,
-          htmlPath: input => (util.htmlPathJoin(path.relative(config.sys.src(), input))),
-          pdfPath: function (input) {
-            const split = input.split(path.sep);
-            split.splice(split.length - 1, 0, 'pdf');
-            const out = path.relative('.' + path.sep + options.srcDir, split.join(path.sep) + '.pdf');
-            return out;
-          }
-        }),
-      $.exec.reporter({ stdout: true }),
-      $.debug({ title: 'Finished Lecture:' }),
-    ], cb);
-  }
+function copyPDFs(cb) {
+  pump([
+    gulp.src(pugs.map(p => (p.replace(/pug$/, 'pdf')))),
+    $.rename({ dirname: '' }),
+    gulp.dest(config.sys.dist('lectures')),
+    $.debug({ title: 'Copied Lecture:' })
+  ], cb);
+}
 
-  function copyPDFs(cb) {
-    pump([
-      gulp.src(pugs.map(p => (p.replace(/pug$/, 'pdf')))),
-      $.rename({ dirname: '' }),
-      gulp.dest(config.sys.dist('lectures')),
-      $.debug({ title: 'Copied Lecture:' })
-    ], cb);
-  }
-
-
-  const tasks = {
+module.exports = {
+  tasks: {
     pdfs: gulp.series(savePDFs, copyPDFs)
-  };
-  _.forEach(tasks, (v, k) => gulp.task(k, v));
-
-  return {
-    tasks: tasks
-  };
+  }
 };
+
+_.forEach(module.exports.tasks, (v, k) => gulp.task(k, v));
